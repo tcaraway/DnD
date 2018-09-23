6240dcafe287ef715a130faeaebaddbe72471959
namhai vu <42756008+namhaivu@users.noreply.github.com>
2018-09-23 16:36:05 -0400

    Added Two tests to DnDCharacterTest
    added givenDnDCharacter_whenClassBard_thenClass and givenDnDCharacter_whenDexMod3_thenkAC

--- DnDCharacterTest.java
+++ DnDCharacterTest.java
@@ -20,5 +20,20 @@ class DnDCharacterTest {
         assertEquals(25, sut.getWisdom());
         assertEquals(30, sut.getCharisma());
     }
+    
+	@Test
+	public void givenDnDCharacter_whenClassBard_thenClass() {
+		DnDCharacter sut = new DnDCharacter();
+		sut.setClass("Bard");
+		assert(sut.getPlayerClass().equals("Bard"));
+	}
+	
+	@Test
+	public void givenDnDCharacter_whenDexMod3_thenkAC() {
+		DnDCharacter sut = new DnDCharacter();
+		sut.setDexMod(3);
+		sut.computeRest();
+		assert(sut.getArmorClass() == 13);
+	}
 
 }
